866587c2b528ead3cc77fda6d44c219088f98800
Andrew Foote <footeandrew1@gmail.com>
2024-10-31 18:28:05 +0000

    set up golden tests

---
+++ tests/test_ruby_marshal_parser.py
@@ -0,0 +1,33 @@
+import functools as ft
+import importlib.resources
+import json
+from pathlib import Path
+import pytest
+import ruby_marshal_parser as marshal
+
+@ft.cache
+def golden_path():
+	with importlib.resources.path('ruby_marshal_parser') as base_path:
+		return base_path.parent / 'tests/golden'
+
+@pytest.fixture
+def update_golden_inputs(request):
+	return request.config.getoption('--update-golden')
+
+def test_to_json_dumpable(update_golden_inputs):
+	for input_path in (golden_path() / 'inputs').iterdir():
+		output = json.dumps(
+			marshal.parse_file(input_path).to_json_dumpable(),
+			indent=2
+		)
+
+		output_path = golden_path() / f'outputs/{input_path.name}'
+
+		if output_path.exists() and not update_golden_inputs:
+			with output_path.open() as output_file:
+				output_content = output_file.read()
+
+			assert output == output_content
+		else:
+			with output_path.open('w') as output_file:
+				output_file.write(output)
